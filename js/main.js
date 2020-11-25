ec5a5195c92d9ab9912a9205444bc106857c8971
A.Dabrowska <aledabrowska@gmail.com>
2020-11-25 14:03:55 +0100

    start

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let pierwsza = "p";
+let druga = "button";
+
+document.getElementById("p");
+document.querySelector('button').onclick = function() {
+    alert('dowolny tekst');
+}
+
+console.log(p);
+console.log(button);
